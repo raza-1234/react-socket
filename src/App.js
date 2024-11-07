037343e183f757ee198fab72fc9970687fe283d3
raza-1234 <razabutt1863@gmail.com>
2024-11-07 13:24:39 +0500

    Build socket connection with server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import io from "socket.io-client";
+
+const socket = io.connect('http://localhost:3001');
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [room, setRoom] = useState('');
+  const [recievedMessage, setRecievedMessage] = useState('');
+
+  const changeHandler = (evt) => {
+    setMessage(evt.target.value);
+  }
+
+  const clickHandler = () => {
+    socket.emit("send-user-message", {room, message})
+  }
+
+  const roomHandler = () => {
+    socket.emit('join_room_space', room)
+  }
+
+  useEffect(() => {
+    socket.on("recieved-message", (data) => {
+      setRecievedMessage(data)
+    })
+  }, [socket])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input calue={room} onChange={(evt) => setRoom(evt.target.value)} placeholder="Enter Room"/>
+      <button onClick={roomHandler}>Room Selected</button>
+      <br/>
+      <br/>
+      <input value={message} placeholder="Enter Message" onChange={changeHandler}/>
+      <button onClick={clickHandler}>Send Message</button>
+      <h2>Message For You</h2>
+      <h4>{recievedMessage}</h4>
     </div>
   );
 }
